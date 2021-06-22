552a6a26c6ddf8281cd4b91e4c621b13cae16390
Dev Arora <cpcdevar@gmail.com>
2021-06-23 04:08:58 +0530

    updates

--- gfgScrapper.js
+++ gfgScrapper.js
@@ -1,22 +1,19 @@
 const fs = require('fs');
+const { page } = require('pdfkit');
 const puppeteer = require('puppeteer');
 let questions = {}
-let mainBrowser;
-let mainPage;
 
 const readline = require("readline");
 
 let rl = readline.createInterface(
     process.stdin,
     process.stdout
-
 )
 
 rl.question("What DSA you want to practice ? ", async function (ans) {
     await pattern532GFG(ans);
     rl.close();
 })
-
 
 async function pattern532GFG(dsaTopic) {
     const browser = await puppeteer.launch({
@@ -36,122 +33,41 @@ async function pattern532GFG(dsaTopic) {
         document.querySelector("div[href='#collapse4'] h4").click();
         document.querySelector("#moreCategories").click();
     })
-
     await pageGFG.waitForSelector("#searchCategories");
     await pageGFG.type("#searchCategories", dsaTopic)
-
     await pageGFG.click('[style="font-size: 12px; padding: 10px; display: block;"]', { delay: 2000 })
     await pageGFG.click("#selectCategoryModal", { delay: 500 })
-
     await pageGFG.click("div[href='#collapse1'] h4", { delay: 2000 });
+    
     await Promise.all([
         pageGFG.waitForNavigation(),
         pageGFG.click("[value='0']", { delay: 2000 })
     ])
-
-
-    let easyQ = await pageGFG.evaluate(function () {
-        let easyQ = {};
-        let problemName = document.querySelectorAll(".panel.problem-block div>span");
-        let problemLink = document.querySelectorAll('[style="position: absolute;top: 0;left: 0;height: 100%;width: 100%;z-index:1;pointer:cursor;"]');
-
-        if (problemName.length > 5) {
-
-            for (let i = 0; i < 5; i++) {
-
-                easyQ[problemName[i].innerText] = problemLink[i].getAttribute("href");
-            }
-        }
-        else {
-            for (let i = 0; i < problemName.length; i++) {
-
-                easyQ[problemName[i].innerText] = problemLink[i].getAttribute("href");
-            }
-        }
-
-
-        return easyQ;
-    })
-
-    questions["Easy Questions: "] = easyQ;
+    await pageGFG.waitForSelector(".panel.problem-block div>span")
+    questions["Easy Questions: "] = await getGFGQuestions(pageGFG, 5);  ;
 
     await pageGFG.click("[value='0']", { delay: 2000 })
-
     await Promise.all([
-        // pageGFG.waitForNavigation(),
+        pageGFG.waitForNavigation(),
         pageGFG.click("[value='1']", { delay: 2000 })
     ])
-
     await pageGFG.waitForSelector(".panel.problem-block div>span")
-
-    let mediumQ = await pageGFG.evaluate(function () {
-        let mediumQ = {};
-        let problemName = document.querySelectorAll(".panel.problem-block div>span");
-        let problemLink = document.querySelectorAll('[style="position: absolute;top: 0;left: 0;height: 100%;width: 100%;z-index:1;pointer:cursor;"]');
-
-        if (problemName.length > 3) {
-
-            for (let i = 0; i < 3; i++) {
-
-                mediumQ[problemName[i].innerText] = problemLink[i].getAttribute("href");
-                console.log(problemLink[i].getAttribute("href"));
-            }
-        }
-        else {
-
-            for (let i = 0; i < problemName.length; i++) {
-
-                mediumQ[problemName[i].innerText] = problemLink[i].getAttribute("href");
-            }
-        }
-
-
-        return mediumQ;
-    })
-
-    questions["Medium Questions: "] = mediumQ
+    questions["Medium Questions: "] =  await getGFGQuestions(pageGFG, 3)
 
     await pageGFG.click("[value='1']", { delay: 2000 })
     await Promise.all([
         pageGFG.waitForNavigation(),
         pageGFG.click("[value='2']", { delay: 2000 })
     ])
-
     await pageGFG.waitForSelector(".panel.problem-block div>span")
-
-    let hardQ = await pageGFG.evaluate(function () {
-        let hardQ = {};
-        let problemName = document.querySelectorAll(".panel.problem-block div>span");
-        let problemLink = document.querySelectorAll('[style="position: absolute;top: 0;left: 0;height: 100%;width: 100%;z-index:1;pointer:cursor;"]');
-
-        if (problemName.length > 2) {
-
-            for (let i = 0; i < 2; i++) {
-
-                hardQ[problemName[i].innerText] = problemLink[i].getAttribute("href");
-            }
-        }
-        else {
-
-            for (let i = 0; i < problemName.length; i++) {
-
-                hardQ[problemName[i].innerText] = problemLink[i].getAttribute("href");
-            }
-        }
-
-
-        return hardQ;
-    })
-
-    questions["Hard Questions: "] = hardQ;
+    questions["Hard Questions: "] =  await getGFGQuestions(pageGFG, 2)
 
     let fileName = dsaTopic + "GFGQuestions.json";
-
     fs.writeFile(fileName, JSON.stringify(questions), function (err) {
         if (err) {
             console.log(err);
         }
-        // browser.close();
+        browser.close();
     })
 
 }
@@ -162,7 +78,7 @@ async function pattern532LC(dsaTopic) {
         headless: false,
         defaultViewport: null,
         args: ["--start-maximized"],
-        slowMo: 75
+        slowMo: 100
     });
 
     let pagesArr = await browser.pages();
@@ -174,136 +90,78 @@ async function pattern532LC(dsaTopic) {
     await pageLC.type("[placeholder='Filter topics']", dsaTopic)
     await pageLC.click(".flex.flex-wrap.py-4.-m-1 span");
 
-    await pageLC.evaluate(function () {
-        document.querySelectorAll(".relative div>button[type='button'][aria-haspopup='true']")[1].click();
-    })
 
-    await pageLC.waitForSelector(".flex.items-center.h-5", { visible: true });
+    questions["Easy Questions: "] = await getLCQuestions(pageLC, 0, 5);
+    questions["Medium Questions: "] = await getLCQuestions(pageLC, 1, 3);
+    questions["Hard Questions: "] = await getLCQuestions(pageLC, 2, 2);
 
-    await pageLC.evaluate(function () {
-            document.querySelectorAll(".flex.items-center.h-5")[0].click();
-    })
-
-    let p= new Promise(function(resolve,reject){
-        setTimeout(function(){
-            resolve();
-        },5000)
-    })
-
-    await p;
-    
-   let easyQ= await pageLC.evaluate(function () {
-        let easyQ = {};
-        let allTrs = document.querySelectorAll("tbody tr");
-
-        if (allTrs.length > 5) {
-            for (let i = 2; i < 7; i++) {
-                let allATags = allTrs[i].querySelectorAll("td a");
-                let problemName = allATags[0].innerText;
-                let problemLink = "https://leetcode.com" + allATags[0].getAttribute("href");;
-                easyQ[problemName] = problemLink;
-            }
-        }
-        else {
-            for (let i = 2; i < allTrs.length; i++) {
-                let allATags = allTrs.querySelectorAll("td a");
-                let problemName = allATags[0].innerText;
-                let problemLink = "https://leetcode.com" + allATags[0].getAttribute("href");
-                easyQ[problemName] = problemLink;
-            }
-        }
-
-        return easyQ;
-    })
-
-    questions["Easy Questions: "]=easyQ;
-
-    await pageLC.evaluate(function () {
-        document.querySelectorAll(".relative div>button[type='button'][aria-haspopup='true']")[1].click();
-    })
-
-    await pageLC.waitForSelector(".flex.items-center.h-5", { visible: true });
-
-    await pageLC.evaluate(function () {
-            document.querySelectorAll(".flex.items-center.h-5")[1].click();
-    })
-
-    p= new Promise(function(resolve,reject){
-        setTimeout(function(){
-            resolve();
-        },5000)
-    })
-
-    await p;
-    
-   let mediumQ= await pageLC.evaluate(function () {
-        let mediumQ = {};
-        let allTrs = document.querySelectorAll("tbody tr");
-
-        if (allTrs.length > 5) {
-            for (let i = 2; i < 5; i++) {
-                let allATags = allTrs[i].querySelectorAll("td a");
-                let problemName = allATags[0].innerText;
-                let problemLink = "https://leetcode.com" + allATags[0].getAttribute("href");;
-                mediumQ[problemName] = problemLink;
-            }
-        }
-        else {
-            for (let i = 2; i < allTrs.length; i++) {
-                let allATags = allTrs.querySelectorAll("td a");
-                let problemName = allATags[0].innerText;
-                let problemLink = "https://leetcode.com" + allATags[0].getAttribute("href");
-                mediumQ[problemName] = problemLink;
-            }
-        }
-
-        return mediumQ;
-    })
-
-    questions["Medium Questions: "]=mediumQ;
-
-    await pageLC.evaluate(function () {
-        document.querySelectorAll(".relative div>button[type='button'][aria-haspopup='true']")[1].click();
-    })
-
-    await pageLC.waitForSelector(".flex.items-center.h-5", { visible: true });
-
-    await pageLC.evaluate(function () {
-            document.querySelectorAll(".flex.items-center.h-5")[2].click();
-    })
-
-    p= new Promise(function(resolve,reject){
-        setTimeout(function(){
-            resolve();
-        },5000)
-    })
-
-    await p;
-    
-   let hardQ= await pageLC.evaluate(function () {
-        let hardQ = {};
-        let allTrs = document.querySelectorAll("tbody tr");
-
-        if (allTrs.length > 5) {
-            for (let i = 2; i < 4; i++) {
-                let allATags = allTrs[i].querySelectorAll("td a");
-                let problemName = allATags[0].innerText;
-                let problemLink = "https://leetcode.com" + allATags[0].getAttribute("href");;
-                hardQ[problemName] = problemLink;
-            }
-        }
-        else {
-            for (let i = 2; i < allTrs.length; i++) {
-                let allATags = allTrs.querySelectorAll("td a");
-                let problemName = allATags[0].innerText;
-                let problemLink = "https://leetcode.com" + allATags[0].getAttribute("href");
-                hardQ[problemName] = problemLink;
-            }
-        }
-
-        return hardQ;
-    })
-
-    questions["Hard Questions: "]=hardQ;
     console.log(questions);
+}
+
+async function getLCQuestions(pageLC, difficulty, numQues) {
+
+    await pageLC.evaluate(function () {
+        document.querySelectorAll(".relative div>button[type='button'][aria-haspopup='true']")[1].click();
+    })
+    await pageLC.waitForSelector(".flex.items-center.h-5", { visible: true });
+    await pageLC.evaluate(function (difficulty) {
+        document.querySelectorAll(".flex.items-center.h-5")[difficulty].click();
+    }, difficulty)
+
+    let p = new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve();
+        }, 5000)
+    })
+
+    await p;
+    return await pageLC.evaluate(function (numQues) {
+        let ques = {};
+        let allTrs = document.querySelectorAll("tbody tr");
+
+        if (allTrs.length >= numQues) {
+            for (let i = 2; i < numQues+2; i++) {
+                let allATags = allTrs[i].querySelectorAll("td a");
+                let problemName = allATags[0].innerText;
+                let problemLink = "https://leetcode.com" + allATags[0].getAttribute("href");
+                ques[problemName] = problemLink;
+            }
+        }
+        else {
+            for (let i = 2; i < allTrs.length; i++) {
+                let allATags = allTrs.querySelectorAll("td a");
+                let problemName = allATags[0].innerText;
+                let problemLink = "https://leetcode.com" + allATags[0].getAttribute("href");
+                ques[problemName] = problemLink;
+            }
+        }
+
+        return ques;
+    }, numQues)
+}
+
+async function getGFGQuestions(pageGFG, numQues) {
+    return await pageGFG.evaluate(function (numQues) {
+        let ques = {};
+        let problemName = document.querySelectorAll(".panel.problem-block div>span");
+        let problemLink = document.querySelectorAll('[style="position: absolute;top: 0;left: 0;height: 100%;width: 100%;z-index:1;pointer:cursor;"]');
+
+        if (problemName.length > numQues) {
+
+            for (let i = 0; i < numQues; i++) {
+
+                ques[problemName[i].innerText] = problemLink[i].getAttribute("href");
+            }
+        }
+        else {
+            for (let i = 0; i < problemName.length; i++) {
+
+                ques[problemName[i].innerText] = problemLink[i].getAttribute("href");
+            }
+        }
+
+
+        return ques;
+
+    }, numQues)
 }
